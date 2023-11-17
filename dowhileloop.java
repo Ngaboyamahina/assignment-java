ac770a890e090dbbb62f5e915817009f538ba82a
Ngaboyamahina <151012671+Ngaboyamahina@users.noreply.github.com>
2023-11-17 10:33:36 +0200

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,9 @@
+class DoWhileLoop{
+   public static void main(String args[]){
+      int num = 0;
+      do{
+         System.out.println("Number: " + num );
+         num = num + 1;
+      }while( num < 10 );
+   }
+}
---
+++ nestedfor.java
@@ -0,0 +1,16 @@
+class NestedFor
+{
+    public static void main(String[] args)
+    {
+        int count = 1;
+        for(int i = 1; i <= 2; i++)
+        {
+            for(int j = 1; j <= 5; j++)
+            {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
